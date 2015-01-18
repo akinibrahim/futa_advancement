43a25dc78078839227cd420aa41e23be27c0627e
akinibrahim <akinibrahim1@gmail.com>
2015-01-18 13:31:38 +0000

    advancment
    advance

---
+++ advanc/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,33 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var row4 = {};	// @container
+	var row1 = {};	// @container
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		$$("navigationView1").goToNextView();
+	};// @lock
+
+	row4.click = function row4_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$("navigationView1").goToView(3);
+	};// @lock
+
+	row1.click = function row1_click (event)// @startlock
+	{// @endlock
+		$$("navigationView1").goToNextView();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("row4", "click", row4.click, "WAF");
+	WAF.addListener("row1", "click", row1.click, "WAF");
+// @endregion
+};// @endlock
---
+++ advanc/WebFolder/notusr.waPage/scripts/index-smartphone.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button3 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+WAF.core.restConnect.defaultService = "cors";
+WAF.core.restConnect.baseURL = "http://wakanda_server_url:application_port";
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		//$$("navigationView1").goToView();
+		 window.location = "/menu.waPage/index-smartphone.html";
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button3", "click", button3.click, "WAF");
+// @endregion
+};// @endlock
